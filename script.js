f5225a3290ebf693222f8699df4b3ff3b18fd508
AlenRaju <raju.alen@yahoo.co.uk>
2023-08-30 12:08:00 +0100

    Java Basics1

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+
 console.log("Hello World");
+
+let figurePrice = "$2";
+figurePrice = "$1.50 for sale";
+
+console.log(figurePrice);
+
+const legoFigure = "Lego Batman";
+console.log(legoFigure);
+
+/* const and let are variables, let is a changeable variable, const (constant) is unchanging. The default is a const variable */
