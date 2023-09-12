1e968bf67098df401d3c3776f52fb74bc4d55ecd
alen raju <whitevortex123@gmail.com>
2023-09-12 17:03:02 +0100

    equality operators,boolean, negation operator

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-console.log("Hello World");
+console.log("Hello World"); 
 
 let figurePrice = "$2";
 figurePrice = "$1.50 for sale";
@@ -9,4 +9,18 @@ console.log(figurePrice);
 const legoFigure = "Lego Batman";
 console.log(legoFigure);
 
-/* const and let are variables, let is a changeable variable, const (constant) is unchanging. The default is a const variable */
+// const and let are variables, let is a changeable variable, const (constant) is unchanging. The default is a const variable
+
+let legoMen = true;
+console.log(legoMen);
+
+legoMen = !true;
+console.log(legoMen);
+
+//  true/false is a boolean data. you can put ! before true it to make it say 'not true'. This is called a negation operator.
+
+const legoWeapons = 20;
+legoWeaponry = legoWeapons !== 25;
+console.log(legoWeaponry);
+
+// we can use === a equality operator to check if two values are equal. We can use !== an inequality operator to check if two values are not equal
