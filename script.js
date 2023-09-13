4c56602e749c4a7428a33165430752fe5edb4ceb
alen raju <raju.alen@yahoo.co.uk>
2023-09-13 17:59:20 +0100

    Inputs

--- script.js
+++ script.js
@@ -24,3 +24,37 @@ legoWeaponry = legoWeapons !== 25;
 console.log(legoWeaponry);
 
 // we can use === a equality operator to check if two values are equal. We can use !== an inequality operator to check if two values are not equal
+
+miles = 24;
+kilometers = 40;
+number = 1.609344;
+
+kilometersToMiles = kilometers/number;
+milesToKilometers = miles*number;
+
+console.log("Distance conversions");
+console.log ("-----------------------");
+console.log(kilometers + " kilometers are:");
+console.log (kilometersToMiles + " miles");
+console.log ("____");
+console.log(miles + " miles are:");
+console.log(milesToKilometers + " kilometers");
+console.log("\n");
+
+celsius = 5;
+fahrenheit = 41;
+
+celsiusToFahrenheit = (celsius * 9/5) + 32;
+fahrenheitToCelsius = (fahrenheit - 32) * 5/9;
+
+console.log("Temperature conversions");
+console.log("-----------------------");
+
+console.log(fahrenheit + " degrees Fahrenheit are:");
+console.log(fahrenheitToCelsius + " degrees Celsius");
+
+console.log("____");
+console.log(celsius + " degrees celsius are:");
+console.log(celsiusToFahrenheit + " degrees fahrenheit");
+
+// changes kilometers to miles and vice versa, celsius to Fahrenheit and vice versa
